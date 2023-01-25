38cccfad6b5491dca17c886041952b71c7f6d054
Greg Daues <daues@illinois.edu>
2023-01-25 10:37:04 -0800

    templates and configs for partial node glideins focused on s3df Slurm cluster

---
+++ etc/config/execConfig.py
@@ -0,0 +1,4 @@
+config.platform.nodeSetRequired = False
+config.platform.localScratch = "/sdf/scratch/$USER_NAME/condor_scratch"
+config.platform.fileSystemDomain = "slac.stanford.edu"
+config.platform.scheduler = "slurm"
---
+++ etc/config/slurmConfig.py
@@ -0,0 +1,4 @@
+config.platform.queue = "$QUEUE"
+config.platform.scratchDirectory = "/sdf/scratch/$USER_NAME/condor_scratch"
+config.platform.loginHostName = "sdfrome001.sdf.slac.stanford.edu"
+config.platform.utilityPath = "/usr/sbin/"
