929c5983a9cb5d18c1c60c9c9cad5e8c54140548
Greg Daues <daues@illinois.edu>
2025-02-25 08:22:25 -0800

    platform config for peakcpus on a node

--- python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
+++ python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
@@ -3,3 +3,4 @@ config.platform.nodeSetRequired = False
 config.platform.localScratch = "$USER_SCRATCH/condor_scratch"
 config.platform.fileSystemDomain = "slac.stanford.edu"
 config.platform.scheduler = "slurm"
+config.platform.peakcpus = 120
