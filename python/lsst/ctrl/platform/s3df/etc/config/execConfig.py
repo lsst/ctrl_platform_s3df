396684296f26ca4a6d16ce215473ef21365915f9
Greg Daues <daues@illinois.edu>
2026-03-23 08:24:01 -0700

    DM-54449: plaform settings for peakmemory, collector

--- python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
+++ python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
@@ -4,3 +4,5 @@ config.platform.localScratch = "$USER_SCRATCH/condor_scratch"
 config.platform.fileSystemDomain = "slac.stanford.edu"
 config.platform.scheduler = "slurm"
 config.platform.peakcpus = 120
+config.platform.peakmemory = 491520
+config.platform.collector = "sdfiana012.sdf.slac.stanford.edu"
