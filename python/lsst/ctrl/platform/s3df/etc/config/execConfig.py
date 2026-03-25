239c2cdc07bf1d108934fcaca175cfd149c56c9b
Greg Daues <daues@illinois.edu>
2026-03-25 10:57:27 -0700

    DM-54449: comments on platform partition configuration

--- python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
+++ python/lsst/ctrl/platform/s3df/etc/config/execConfig.py
@@ -4,5 +4,8 @@ config.platform.localScratch = "$USER_SCRATCH/condor_scratch"
 config.platform.fileSystemDomain = "slac.stanford.edu"
 config.platform.scheduler = "slurm"
 config.platform.peakcpus = 120
+# This is the value for nodes of the milano partition.
+# Configuration per partition will be implemented in a
+# followup ticket.
 config.platform.peakmemory = 491520
 config.platform.collector = "sdfiana012.sdf.slac.stanford.edu"
